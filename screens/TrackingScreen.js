151ba590255a104477959ab407f259b10f1df79b
Rakesh Kumar Mishra <rakesh.mishra@instape.com>
2025-01-15 12:51:34 +0530

    Added Google Map Feature in project:

--- screens/TrackingScreen.js
+++ screens/TrackingScreen.js
@@ -2,12 +2,26 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 const TrackingScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Real-time Tracking</Text>
       <Text style={styles.message}>Tracking feature is under development.</Text>
+      <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+      <Text>map is not working</Text>
+     </MapView>
       {/* You can integrate a map component here for real-time tracking */}
     </View>
   );
@@ -29,6 +43,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'center',
   },
+  map: {
+    flex: 1
+  }
 });
 
 export default TrackingScreen;
